e000be16b82581e0a92fe450584eff283f4fb751
Vasily93 <markovavasily@gmail.com>
2022-01-17 13:55:55 -0800

    basic MUI layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Album from './pages/Album';
 
 function App() {
   return (
     <div className="App">
-      Personal Website with React and Notion API
+      <Album />
     </div>
   );
 }
