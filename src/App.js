0d5f4e3e9d1e3e98af65a01a0211269d3be3c3a3
Vasily93 <markovavasily@gmail.com>
2022-01-17 13:31:48 -0800

    init setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Personal Website with React and Notion API
+    </div>
+  );
+}
+
+export default App;
